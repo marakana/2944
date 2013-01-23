8a9219f8ab00d9478780bd5c386eabb28b301ed3
Marko Gargenta <marko@marakana.com>
2013-01-22 16:28:18 -0800

    Implemented the lab with textCount

--- QYamba/src/com/qcom/yamba/StatusActivity.java
+++ QYamba/src/com/qcom/yamba/StatusActivity.java
@@ -1,16 +1,22 @@
 package com.qcom.yamba;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class StatusActivity extends Activity {
+	private int defaultTextColor;
 	private Button buttonUpdate;
 	private EditText textStatus;
+	private TextView textCount;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +25,40 @@ public class StatusActivity extends Activity {
 
 		buttonUpdate = (Button) findViewById(R.id.button_update);
 		textStatus = (EditText) findViewById(R.id.text_status);
+		textCount = (TextView) findViewById(R.id.text_count);
+		defaultTextColor = textCount.getTextColors().getDefaultColor();
 
 		buttonUpdate.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				String status = textStatus.getText().toString(); 
+				String status = textStatus.getText().toString();
 				Log.d("Yamba", "button clicked: " + status);
+			}
+
+		});
+
+		textStatus.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				int count = 140 - s.length();
+				textCount.setText(Integer.toString(count));
+
+				if (count < 50) {
+					textCount.setTextColor(Color.RED);
+				} else {
+					textCount.setTextColor(defaultTextColor);
+				}
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
 			}
 
 		});
