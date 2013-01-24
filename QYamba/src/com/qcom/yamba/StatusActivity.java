ae0b31f46555e3bcca064c41e8207119ddd84063
Marko Gargenta <marko@marakana.com>
2013-01-24 13:47:45 -0800

    Fixed bug

--- QYamba/src/com/qcom/yamba/StatusActivity.java
+++ QYamba/src/com/qcom/yamba/StatusActivity.java
@@ -11,9 +11,9 @@ public class StatusActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		getFragmentManager().beginTransaction().add(new StatusFragment(),
-				"Status Fragment").commit();
+		setContentView(R.layout.activity_status);
+		getFragmentManager().beginTransaction()
+				.add(R.id.container, new StatusFragment(), "Status Fragment").commit();
 
 		if (Build.VERSION.SDK_INT > Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
 			getActionBar().setDisplayHomeAsUpEnabled(true);
